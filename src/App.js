43c81b7efcfb7e674ef483167c01ea7b752cc06e
Syednazeerahmed <naz224598@gmail.com>
2024-03-09 12:28:09 +0530

    ...

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import './App.css';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { Navbar } from './components/navbar';
-import { Main } from './pages/main';
 import { Login } from "./pages/login/login";
 import { Entry } from './pages/Entry/entry';
 import { AllEntries } from './pages/All-Entries/allEntries';
@@ -13,8 +12,8 @@ function App() {
         <Navbar/>
         <Routes>
           <Route path='/' element={<Login />}/>
-          <Route path='entry' element={<Entry />}/>
-          <Route path='allEntries' element={<AllEntries />} />
+          <Route path='/entry' element={<Entry />}/>
+          <Route path='/allEntries' element={<AllEntries />} />
         </Routes>
       </Router>
     </div>
